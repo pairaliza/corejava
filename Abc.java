f2a13d40ff701b059771b78402597b3d51da33ba
liza <pariraliza@gmail.com>
2022-07-27 12:15:25 +0530

    Formatted the Code

--- Abc.java
+++ Abc.java
@@ -1,32 +1,22 @@
-class Abc 
-{
-	int i =0;
-	int j =0;
-	static int k =0;
+class Abc {
+	int i = 0;
+	int j = 0;
+	static int k = 0;
 
-	void fun1(){
-    System.out.println("Inside  fun1()");
+	void fun1() {
+		System.out.println("Inside  fun1()");
 	}
 
-	void fun2(){
-		
-     System.out.println("Inside  fun2()");
+	void fun2() {
+
+		System.out.println("Inside  fun2()");
 	}
-	
-	static void fun3(){
-    System.out.println("Inside static fun3()");
+
+	static void fun3() {
+		System.out.println("Inside static fun3()");
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public static void main(String[] args) 
-	{
+
+	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
 }
