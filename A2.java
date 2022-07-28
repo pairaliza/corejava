ae8ace18dff8313854fb0d776351878ca16f8494
liza <pariraliza@gmail.com>
2022-07-28 14:47:04 +0530

    I am commited

--- A2.java
+++ A2.java
@@ -1,12 +1,7 @@
-class A
+class A2 
 {
-	int x= 0;
 	public static void main(String[] args) 
 	{
-		int y= 20;
-		y= y+1;
-		x= x+1;
 		System.out.println("Hello World!");
-		System.out.println(y);
 	}
 }
